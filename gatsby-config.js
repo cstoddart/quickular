0576d891e59235d7eabb823a08684c9a9e133988
Chris Stoddart <cstoddart@rocketmail.com>
2019-11-29 23:16:49 -0600

    update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,6 @@ require("dotenv").config();
 module.exports = {
   siteMetadata: {
     title: 'Quickular',
-    description: 'A better way to make group decisions.',
-    author: '@stodnstove',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -30,10 +28,10 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
+        background_color: '#002211',
+        theme_color: '#002211',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/favicon.png', // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
